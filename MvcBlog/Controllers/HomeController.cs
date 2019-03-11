dd77446c37fddd48c33c279efa1189385885e8a6
Jerry <contactmevia@githuborothers.com>
2019-03-11 09:47:37 -0500

    DisplayOnHomePage-NeedToSliceBody+ModifyEditPost-DisplayPrevInfo

--- MvcBlog/Controllers/HomeController.cs
+++ MvcBlog/Controllers/HomeController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using MvcBlog.Models;
+using MvcBlog.Models.ViewModels;
+using PostDatabase.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +13,31 @@ namespace MvcBlog.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext DbContext;
+
+        public HomeController()
+        {
+            DbContext = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Message = "Your home page.";
+
+            var appUserId = User.Identity.GetUserId();
+
+            var model = DbContext.Posts
+                .Where(p => p.UserId == appUserId)
+                .Select(p => new IndexHomeViewModel
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Body = p.Body,
+                    DateCreated=p.DateCreated,
+                    MediaUrl=p.MediaUrl
+                }).ToList();
+
+            return View(model);
         }
 
         public ActionResult About()
